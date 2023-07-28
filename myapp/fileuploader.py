79bc18f5ca547292d6ff4529cb60dd8a3ea5d94a
psmxnpatrick <nekelpatrick@gmail.com>
2023-07-28 01:10:30 -0300

    enhance fileupload performance

--- myapp/fileuploader.py
+++ myapp/fileuploader.py
@@ -1,12 +1,12 @@
-import time
-from dropbox import Dropbox, exceptions
+from concurrent.futures import ThreadPoolExecutor
 import os
+import dropbox
 from datetime import datetime
 
 
 class FileUploader:
-    def __init__(self, local_folder_path, dropbox_folder_path, token):
-        self.dbx = Dropbox(token)
+    def __init__(self, local_folder_path, dropbox_folder_path):
+        self.dbx = dropbox.Dropbox(os.getenv('DROPBOX_TOKEN'))
         self.local_folder_path = local_folder_path
         self.dropbox_folder_path = dropbox_folder_path
 
@@ -26,41 +26,19 @@ class FileUploader:
             "Novembro",
             "Dezembro",
         ]
-        current_month_index = time.localtime().tm_mon  # Month index is 1-based
-        return months_in_pt_br[current_month_index - 1]
+        current_month_index = datetime.now().month - 1
+        return months_in_pt_br[current_month_index]
 
-    def get_or_create_folder(self):
-        month_name = self.get_current_month_name()
-        folder_name = month_name
-        counter = 1
-
-        while True:
-            try:
-                self.dbx.files_get_metadata(os.path.join(
-                    self.dropbox_folder_path, folder_name))
-                folder_name = f"{month_name}{counter}"
-                counter += 1
-            except exceptions.ApiError as error:
-                if error.error.is_path() and error.error.get_path().is_not_found():
-                    self.dbx.files_create_folder_v2(os.path.join(
-                        self.dropbox_folder_path, folder_name))
-                    break
-                raise error
-
-        return folder_name
+    def upload_file(self, file_name):
+        local_file_path = os.path.join(self.local_folder_path, file_name)
+        with open(local_file_path, 'rb') as local_file:
+            file_content = local_file.read()
+            dropbox_file_path = f"{self.dropbox_folder_path}/{self.folder_name}/{file_name}"
+            self.dbx.files_upload(file_content, dropbox_file_path)
+        # Delete local file
+        os.remove(local_file_path)
 
     def upload_files(self):
-        files = os.listdir(self.local_folder_path)
-        folder_name = self.get_or_create_folder()
-
-        for file in files:
-            file_path = os.path.join(self.local_folder_path, file)
-            with open(file_path, "rb") as f:
-                file_data = f.read()
-            dropbox_path = os.path.join(
-                self.dropbox_folder_path, folder_name, file)
-
-            try:
-                self.dbx.files_upload(file_data, dropbox_path)
-            except exceptions.ApiError as error:
-                print(f"Failed to upload {file} to Dropbox: {error}")
+        self.folder_name = self.get_current_month_name()
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            executor.map(self.upload_file, os.listdir(self.local_folder_path))
