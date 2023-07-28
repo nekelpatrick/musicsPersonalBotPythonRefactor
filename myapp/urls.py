d87b5d499e7ac51d70536677c86652bf76c35225
psmxnpatrick <nekelpatrick@gmail.com>
2023-07-27 21:23:10 -0300

    clean unnecessary code

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,15 +1,9 @@
 from django.urls import path
-from rest_framework.routers import SimpleRouter
-from .views import ItemViewSet
 from .views import DownloadView
 
 
 
 
-router = SimpleRouter()
-router.register('items', ItemViewSet, basename='items')
-
-urlpatterns = router.urls
 
 urlpatterns = [
     path('download/', DownloadView.as_view(), name='download'),
