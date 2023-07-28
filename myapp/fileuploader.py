d70b4d7aecad2ba69e68699e0030b0b5695f6765
psmxnpatrick <nekelpatrick@gmail.com>
2023-07-27 23:51:01 -0300

    improve fileUploader usability

--- myapp/fileuploader.py
+++ myapp/fileuploader.py
@@ -1,33 +1,66 @@
-import os
-import dropbox
-from dropbox.exceptions import AuthError, ApiError
-from concurrent.futures import ThreadPoolExecutor
 import time
+from dropbox import Dropbox, exceptions
+import os
+from datetime import datetime
+
 
 class FileUploader:
-    def __init__(self, local_folder_path, dropbox_folder_path, dropbox_token):
-        self.dbx = dropbox.Dropbox(dropbox_token)
+    def __init__(self, local_folder_path, dropbox_folder_path, token):
+        self.dbx = Dropbox(token)
         self.local_folder_path = local_folder_path
         self.dropbox_folder_path = dropbox_folder_path
 
-    def upload_file(self, file_name):
-        local_file_path = os.path.join(self.local_folder_path, file_name)
-        dropbox_file_path = f"{self.dropbox_folder_path}/{file_name}"
-        with open(local_file_path, "rb") as file:
-            try:
-                self.dbx.files_upload(file.read(), dropbox_file_path)
-            except ApiError as e:
-                if e.error.is_path() and \
-                        e.error.get_path().is_conflict():
-                    print(f"Conflict with file: {file_name}.")
-                elif e.user_message_text:
-                    print(e.user_message_text)
-                else:
-                    print(e)
-            except AuthError as e:
-                print(f"Authentication error: {e}")
+    @staticmethod
+    def get_current_month_name():
+        months_in_pt_br = [
+            "Janeiro",
+            "Fevereiro",
+            "Março",
+            "Abril",
+            "Maio",
+            "Junho",
+            "Julho",
+            "Agosto",
+            "Setembro",
+            "Outubro",
+            "Novembro",
+            "Dezembro",
+        ]
+        current_month_index = time.localtime().tm_mon  # Month index is 1-based
+        return months_in_pt_br[current_month_index - 1]
 
-    def upload_files(self, max_workers=5):
+    def get_or_create_folder(self):
+        month_name = self.get_current_month_name()
+        folder_name = month_name
+        counter = 1
+
+        while True:
+            try:
+                self.dbx.files_get_metadata(os.path.join(
+                    self.dropbox_folder_path, folder_name))
+                folder_name = f"{month_name}{counter}"
+                counter += 1
+            except exceptions.ApiError as error:
+                if error.error.is_path() and error.error.get_path().is_not_found():
+                    self.dbx.files_create_folder_v2(os.path.join(
+                        self.dropbox_folder_path, folder_name))
+                    break
+                raise error
+
+        return folder_name
+
+    def upload_files(self):
         files = os.listdir(self.local_folder_path)
-        with ThreadPoolExecutor(max_workers=max_workers) as executor:
-            executor.map(self.upload_file, files)
+        folder_name = self.get_or_create_folder()
+
+        for file in files:
+            file_path = os.path.join(self.local_folder_path, file)
+            with open(file_path, "rb") as f:
+                file_data = f.read()
+            dropbox_path = os.path.join(
+                self.dropbox_folder_path, folder_name, file)
+
+            try:
+                self.dbx.files_upload(file_data, dropbox_path)
+            except exceptions.ApiError as error:
+                print(f"Failed to upload {file} to Dropbox: {error}")
