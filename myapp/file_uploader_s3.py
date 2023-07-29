040a97972e6b13f4b134411f6ffb949493152ce5
Patrick Nekel <nekelpatrick@gmail.com>
2023-07-29 20:40:41 -0300

    add month folder feature

--- myapp/file_uploader_s3.py
+++ myapp/file_uploader_s3.py
@@ -1,29 +1,44 @@
 from concurrent.futures import ThreadPoolExecutor
 import os
 import boto3
-from botocore.exceptions import NoCredentialsError, BotoCoreError
-from boto3.s3.transfer import S3UploadFailedError
+from botocore.exceptions import NoCredentialsError
+from datetime import datetime
 
 
 class FileUploader:
     def __init__(self, aws_access_key_id, aws_secret_access_key, bucket_name):
         self.s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id,
-                               aws_secret_access_key=aws_secret_access_key,
-                               region_name='sa-east-1')  # include your region here
+                               aws_secret_access_key=aws_secret_access_key, region_name='sa-east-1')
         self.bucket_name = bucket_name
+
+    @staticmethod
+    def get_current_month_name():
+        months_in_pt_br = [
+            "Janeiro",
+            "Fevereiro",
+            "Março",
+            "Abril",
+            "Maio",
+            "Junho",
+            "Julho",
+            "Agosto",
+            "Setembro",
+            "Outubro",
+            "Novembro",
+            "Dezembro",
+        ]
+        current_month_index = datetime.now().month - 1
+        return months_in_pt_br[current_month_index]
 
     def upload_file(self, file_name, folder_path):
         try:
             print(f"Uploading {file_name}...")
-            self.s3.upload_file(os.path.join(
-                folder_path, file_name), self.bucket_name, file_name)
+            folder_name = self.get_current_month_name()
+            self.s3.upload_file(os.path.join(folder_path, file_name),
+                                self.bucket_name, f'{folder_name}/{file_name}')
             print(f"{file_name} uploaded successfully")
         except NoCredentialsError:
             print("Credentials not available")
-        except BotoCoreError as e:
-            print(f"BotoCoreError: {e}")
-        except S3UploadFailedError as e:
-            print(f"S3UploadFailedError: {e}")
 
     def upload_files(self, folder_path):
         print("Starting upload...")
